bf718102bdafc85fbe869825c0bbb0616083c9e0
Rodrigo1163 <rodrigo.sena1163@gmail.com>
2023-12-18 21:22:18 -0400

    adicionei requisições para o calculo

--- assets/js/style.js
+++ assets/js/style.js
@@ -15,17 +15,54 @@ function theme() {
     }
 }
 
-function criaCalculadora(){
-    return{
-        inputDisplay: document.querySelector('#display'),
-        buttons: document.querySelectorAll('.btn'),
-        inicia(){
+function Calculadora(){
+    this.display = document.querySelector('#display');
+    
+    this.inicia = () => {
+        this.capituraCliques();
+        this.capituraEnter();
+    };
 
-        },
-        display(valor){
-            this.inputDisplay.innerHTML = valor
-        },
-        
+    this.capituraEnter = () => {
+        document.addEventListener('keyup', e => {
+            if(e.keyCode !== 13) return;
+            this.realizaConta();
+        })
+    };
+    this.capituraCliques = () => {
+        document.addEventListener('click', e => {
+            const el = e.target;
+
+            if(el.classList.contains('btnNumber')) this.addNumDisplay(el);
+            if(el.classList.contains('btnReset')) this.reset();
+            if(el.classList.contains('btnDel')) this.del();
+            if(el.classList.contains('btnEq')) this.realizaConta();
+        });
     }
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+        this.display.focus();
+    }
+
+    this.reset = () => this.display.value = "";
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+
+            if(!conta){
+                alert('conta inválida')
+                return;
+            }
+            this.display.value = conta;
+        } catch(e){
+            alert('conta inválida');
+            return
+        }
+    }
+    
 }
+
+const calculadora = new Calculadora();
+calculadora.inicia();
 
